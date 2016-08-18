31ab9f62fea035fcbcc270f228231b27896d8484
Jean Ureña <juanpedro1519@gmail.com>
2016-08-18 10:34:34 -0400

    User system with my code

--- pueba-jp/blog.py
+++ pueba-jp/blog.py
@@ -89,16 +89,13 @@ def check_secure_val(h):
 class Handler(webapp2.RequestHandler) :
 
 	def write(self, *a, **kw) :
-
 		self.response.out.write(*a, **kw)
 
 	def render_str(self, template, **params) :
-
 		t = jinja_env.get_template(template)
 		return t.render(params)
 
 	def render(self, template, **kw) :
-
 		self.write(self.render_str(template, **kw))
 
 # models
